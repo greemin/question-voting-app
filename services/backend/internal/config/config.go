4c30b1340624bfb4bb15e0862b949eb302c90e93
greemin <karl@jotech.net>
2026-03-18 21:37:23 +0100

    big configuration warning

--- services/backend/internal/config/config.go
+++ services/backend/internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"log"
 	"os"
 	"strings"
 )
@@ -13,6 +14,20 @@ type Config struct {
 }
 
 func Load() *Config {
+	env := strings.ToLower(os.Getenv("ENV"))
+
+	if env != "production" {
+		log.Print("\n\n" +
+			"\033[31m*****************************************************************\033[0m\n" +
+			"\033[31m* WARNING: You are running the application in DEVELOPMENT mode! *\033[0m\n" +
+			"\033[31m* This setup is NOT SECURE and should NOT be used in production.*\033[0m\n" +
+			"\033[31m* To run in production, set the ENV environment variable to     *\033[0m\n" +
+			"\033[31m* 'production' and ensure all other environment variables       *\033[0m\n" +
+			"\033[31m* (e.g. MONGO_URI, CORS_ORIGINS, PORT) are configured securely. *\033[0m\n" +
+			"\033[31m*****************************************************************\033[0m\n" +
+			"\n")
+	}
+
 	return &Config{
 		Port:         getEnvOrDefault("PORT", "8081"),
 		MongoURI:     getEnvOrDefault("MONGO_URI", "mongodb://devroot:devpassword@localhost:27017"),
