079d5d13a42de51b78823209ad0e50babd86f00b
greemin <karl@jotech.net>
2026-04-15 19:27:47 +0200

    add github actions

--- services/frontend/playwright.config.ts
+++ services/frontend/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL: process.env.BASE_URL ?? 'http://localhost:5173',
     
     // Collect trace when retrying the failed test.
     trace: 'on-first-retry',
